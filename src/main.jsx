3dca8b0029087ab1252241d9641c4fea439aec22
Roses Burgers <roses@burgers.com>
2026-03-23 13:10:08 -0300

    Revert: remove online orders tab from admin app

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
-import PaginaPedido from './PaginaPedido.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/pedido" element={<PaginaPedido />} />
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 )
